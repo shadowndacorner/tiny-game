edc2051b31c944f5332dbea89c59ca5b4433cf1b
Ian Diaz <shadowndacorner@gmail.com>
2018-07-17 00:47:04 -0500

    Added GLFW (SDL is way too big), might swap it out for straight winapi later on

--- game/platform/windows/main.cpp
+++ game/platform/windows/main.cpp
@@ -1,90 +1,36 @@
 #include <game_core.hpp>
-#include "win_platform.h"
-#include <tchar.h>
 #include <stdio.h>
+#include <GLFW/glfw3.h>
 
-// Define these here to minimize typos, or preferably, load them from a
-// resource file at the top of the main function
-#define MYCLASSNAME    TEXT("MainWndClass")
-#define MYWINDOWNAME   TEXT("Main Window")
+#undef main
 
-// Global variable to keep track of your hInstance
-HINSTANCE g_hInstance;
-
-static bool shouldDestroy = false;
-LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-	printf("Received message %d\n", uMsg);
-	switch (uMsg)
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 	{
-	case WM_CLOSE:
-		printf("CLOSE ME!\n");
-		shouldDestroy = true;
-		break;
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
 	}
-
-	// If you don't process any of the messages yourself, you
-	// must pass them to DefWindowProc for default handling.
-	return DefWindowProc(hWnd, uMsg, wParam, lParam);
-}
-
-int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-	LPTSTR lpCmdLine, int nCmdShow)
-{
-	// Save the instance handle in a global variable.
-	g_hInstance = hInstance;
-
-	// Register your window class.
-	// (A full-featured app will probably want to set additional members.)
-	WNDCLASSEX wcex = { 0 };
-	wcex.cbSize = sizeof(wcex);
-	wcex.lpfnWndProc = WndProc;
-	wcex.hInstance = hInstance;
-	wcex.lpszClassName = MYCLASSNAME;
-	if (!RegisterClassEx(&wcex))
-	{
-		MessageBox(NULL, TEXT("Call to RegisterClassEx failed!"), NULL, MB_OK);
-		return 1;
-	}
-
-	// Create your main window.
-	HWND hwndMain = CreateWindowEx(0, MYCLASSNAME, MYWINDOWNAME, WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL,
-		hInstance, NULL);
-	if (!hwndMain)
-	{
-		MessageBox(NULL, TEXT("Call to CreateWindowEx failed!"), NULL, MB_OK);
-		return 1;
-	}
-
-	// Show your main window.
-	ShowWindow(hwndMain, nCmdShow);
-	UpdateWindow(hwndMain);
-
-	// Run the main message loop.
-	BOOL bRetVal;
-	MSG msg;
-	while ((bRetVal = GetMessage(&msg, NULL, 0, 0)) != 0)
-	{
-		if (bRetVal == -1)
-		{
-			MessageBox(NULL, TEXT("Error encountered in message loop!"), NULL, MB_OK);
-			return 1;
-		}
-		else
-		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
-
-		if (shouldDestroy)
-			break;
-	}
-
-	return (int)msg.wParam;
 }
 
 int main(int argc, char** argv)
 {
-	return WinMain(0, 0, GetCommandLine(), argc);
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	auto window = glfwCreateWindow(1280, 720, "TinyGame", NULL, NULL);
+
+	glfwSetKeyCallback(window, key_callback);
+	
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		
+		// game code
+
+		// clear
+		// rendering code
+		glfwSwapBuffers(window);
+	}
+	glfwTerminate();
+	return 0;
 }
